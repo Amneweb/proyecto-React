01cd0e076a1ae0c40495136f58119b8e48ff8212
Amneris Calle <amneris.calle@gmail.com>
2023-12-09 07:01:56 -0300

    página No encontrado corregida

--- src/components/NoEncontrado/NoEncontrado.jsx
+++ src/components/NoEncontrado/NoEncontrado.jsx
@@ -1,6 +1,16 @@
+import notFound from './notFound.png';
 const NoEncontrado = () => {
   return (
-    <div>NoEncontrado</div>
+    <div className='container text-center'> 
+    <div className='row justify-content-center'>
+      <div className="col col-lg-4">
+     <h1 className='h__notfound'>ERROR 404</h1>
+     <img className='img__notfound' src={notFound} alt="imagen de No encontrado" />
+     <h2 className='h__notfound'>Página no encontrada</h2>
+     <p>Lo sentimos, no encontramos lo que buscabas.</p>
+      </div>
+      </div>
+      </div>
   )
 }
 
