2c5b908357a8f123f9a477a771eed5d0fe87840b
Amneris Calle <amneris.calle@gmail.com>
2023-12-20 23:21:35 -0300

    conexión con firebase para cargar datos

--- src/components/NoEncontrado/NoEncontrado.jsx
+++ src/components/NoEncontrado/NoEncontrado.jsx
@@ -3,7 +3,7 @@ const NoEncontrado = () => {
   return (
     <div className='container text-center'> 
     <div className='row justify-content-center'>
-      <div className="col col-lg-4">
+      <div className="col col-lg-6">
      <h1 className='h__notfound'>ERROR 404</h1>
      <img className='img__notfound' src={notFound} alt="imagen de No encontrado" />
      <h2 className='h__notfound'>Página no encontrada</h2>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import './fonts/fonts.css';
-import './App.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./helpers/subidaAfirebase.js";
+import "./fonts/fonts.css";
+import "./App.css";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAp2QWYZa2Si8VJaWBIkYs21n8VuVjgYys",
+  authDomain: "mw-libros.firebaseapp.com",
+  projectId: "mw-libros",
+  storageBucket: "mw-libros.appspot.com",
+  messagingSenderId: "424330078680",
+  appId: "1:424330078680:web:e040157af19f1edcbcaaad",
+};
+
+const appFire = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
