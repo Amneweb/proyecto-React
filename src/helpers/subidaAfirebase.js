7fb7cbfc34254d725eb65fb6deedde25db0d0e9b
Amneris Calle <amneris.calle@gmail.com>
2024-01-01 17:41:17 -0300

    datos subidos a Firebase

--- src/helpers/subidaAfirebase.js
+++ src/helpers/subidaAfirebase.js
@@ -1,39 +1,62 @@
 import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
-import dataAutoresFire from "../data/dataAutoresFire.json";
-const firebaseConfig = {
-  apiKey: "AIzaSyAp2QWYZa2Si8VJaWBIkYs21n8VuVjgYys",
-  authDomain: "mw-libros.firebaseapp.com",
-  projectId: "mw-libros",
-  storageBucket: "mw-libros.appspot.com",
-  messagingSenderId: "424330078680",
-  appId: "1:424330078680:web:e040157af19f1edcbcaaad",
+import dataBooks from "../data/dataBooks.json";
+import dataCategorias from "../data/dataCategorias.json";
+import dataSagas from "../data/dataSagas.json";
+import dataAutores from "../data/dataAutores.json";
+import dataCarousel from "../data/dataCarousel.json";
+
+export const SubidaAfire = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAp2QWYZa2Si8VJaWBIkYs21n8VuVjgYys",
+    authDomain: "mw-libros.firebaseapp.com",
+    projectId: "mw-libros",
+    storageBucket: "mw-libros.appspot.com",
+    messagingSenderId: "424330078680",
+    appId: "1:424330078680:web:e040157af19f1edcbcaaad",
+  };
+
+  initializeApp(firebaseConfig);
+
+  const subirLibros = () => {
+    const db = getFirestore();
+    const librosCollection = collection(db, "libros");
+
+    dataBooks.forEach((libro) => addDoc(librosCollection, libro));
+  };
+  subirLibros();
+
+  const subirCategorias = () => {
+    const db = getFirestore();
+    const categoriasCollection = collection(db, "categorias");
+
+    dataCategorias.forEach((categoria) =>
+      addDoc(categoriasCollection, categoria)
+    );
+  };
+  subirCategorias();
+
+  const subirAutores = () => {
+    const db = getFirestore();
+    const autoresCollection = collection(db, "autores");
+
+    dataAutores.forEach((autor) => addDoc(autoresCollection, autor));
+  };
+  subirAutores();
+
+  const subirSagas = () => {
+    const db = getFirestore();
+    const sagasCollection = collection(db, "sagas");
+
+    dataSagas.forEach((saga) => addDoc(sagasCollection, saga));
+  };
+  subirSagas();
+
+  const subirCarousel = () => {
+    const db = getFirestore();
+    const carouselCollection = collection(db, "carousel");
+
+    dataCarousel.forEach((carousel) => addDoc(carouselCollection, carousel));
+  };
+  subirCarousel();
 };
-
-initializeApp(firebaseConfig);
-
-// const subirLibros = () => {
-//   const db = getFirestore();
-//   const librosCollection = collection(db, "libros");
-
-//   dataBooksFire.forEach((libro) => addDoc(librosCollection, libro));
-// };
-// subirLibros();
-
-// const subirCategorias = () => {
-//   const db = getFirestore();
-//   const categoriasCollection = collection(db, "categorias");
-
-//   dataCategoriasFire.forEach((categoria) =>
-//     addDoc(categoriasCollection, categoria)
-//   );
-// };
-// subirCategorias();
-
-const subirAutores = () => {
-  const db = getFirestore();
-  const autoresCollection = collection(db, "autores");
-
-  dataAutoresFire.forEach((autor) => addDoc(autoresCollection, autor));
-};
-subirAutores();
