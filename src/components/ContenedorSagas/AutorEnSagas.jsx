1463bd12606ab13552748d30b2dc2371976ac9a7
Amneris Calle <amneris.calle@gmail.com>
2024-01-11 22:39:26 -0300

    corrigiendo sagas

---
+++ src/components/ContenedorSagas/AutorEnSagas.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useFilteredCollections } from "../../hooks/useFilteredCollections";
+import noDisponible from "../ContenedorAutor/assets/noDisponible.png";
+import imagenesAutores from "../../helpers/imagenesAutores";
+const AutorEnSagas = ({ id }) => {
+  const datosAutor = useFilteredCollections("autores", "id", "==", id);
+  return (
+    <div className="row align-items-center">
+      {datosAutor ? (
+        <>
+          <div className="contenedor__autorSagas col-3 g-4 px-4">
+            <img
+              src={
+                imagenesAutores.find(
+                  ({ id }) => Number(id) === datosAutor[0].id
+                )
+                  ? imagenesAutores.find(
+                      ({ id }) => Number(id) === datosAutor[0].id
+                    ).ruta
+                  : noDisponible
+              }
+              alt={`Foto de ${datosAutor[0].nombre}`}
+            />
+          </div>
+          <div className="col-9 g-4">
+            <p>{datosAutor[0].biografia} </p>
+          </div>
+        </>
+      ) : (
+        <p>Cargando datos ...</p>
+      )}
+    </div>
+  );
+};
+
+export default AutorEnSagas;
--- src/components/ContenedorSagas/ContenedorSagas.jsx
+++ src/components/ContenedorSagas/ContenedorSagas.jsx
@@ -1,80 +1,49 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import {
-  fetchDatosSagas,
-  fetchLibrosDeSagas,
-} from "../../helpers/fetchDatosSagas";
 import ItemList from "../ItemList/ItemList";
 import Loader from "../Loader/Loader";
 import { useParams } from "react-router-dom";
-import imagenesAutores from "../../helpers/imagenesAutores";
-import noDisponible from "../ContenedorAutor/assets/noDisponible.png";
+import { useFilteredCollections } from "../../hooks/useFilteredCollections";
+import AutorEnSagas from "./AutorEnSagas";
+
 const ContenedorSagas = () => {
   const saga = useParams().saga;
 
-  const [loader, setLoader] = useState(true);
-  const [libros, setLibros] = useState([]);
-  const [datosSaga, setDatosSaga] = useState({});
-  const [datosAutor, setDatosAutor] = useState({});
-  const [errorStatus, setErrorStatus] = useState(null);
-  useEffect(() => {
-    setLoader(true);
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    Promise.all([fetchDatosSagas(saga), fetchLibrosDeSagas(saga)])
-      .then(([[sagaSeleccionada, autorSaga], librosSaga]) => {
-        setLibros(librosSaga);
-        setDatosSaga(sagaSeleccionada[0]);
-        setDatosAutor(autorSaga[0]);
-      })
-
-      .catch((error) => {
-        setErrorStatus(error.message);
-      })
-      .finally(() => {
-        setLoader(false);
-      });
-  }, [saga]);
-
+  const datosSaga = useFilteredCollections("sagas", "id", "==", Number(saga));
+  const libros = useFilteredCollections("libros", "saga", "==", Number(saga));
+  console.log("saga datos ", datosSaga);
   return (
     <section className="container sagas-en-index">
-      {loader && <Loader />}
-      {errorStatus && <div>{errorStatus}</div>}
-      <div className="row">
-        <h2>
-          Saga <span className="termino-busqueda">"{datosSaga.nombre}"</span>
-        </h2>
-        <p>{datosSaga.descripcion} </p>
-        <p>
-          Autor: <span className="termino-busqueda">{datosAutor.nombre}</span>
-        </p>
-        <p>
-          Cantidad de tomos:{" "}
-          <span className="termino-busqueda">{datosSaga.cantidad}</span>
-        </p>
-
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-          <ItemList libros={libros} />
+      {datosSaga ? (
+        <div className="row">
+          <h2>
+            Saga{" "}
+            <span className="termino-busqueda">"{datosSaga[0].nombre}"</span>
+          </h2>
+          <p>{datosSaga[0].descripcion} </p>
+          <p>
+            Autor:{" "}
+            <span className="termino-busqueda">
+              {datosSaga[0].autor.nombre}
+            </span>
+          </p>
+          <p>
+            Cantidad de tomos:{" "}
+            <span className="termino-busqueda">{datosSaga[0].cantidad}</span>
+          </p>
         </div>
-        <div className="row mt-5">
-          <h2>Sobre el autor</h2>
-        </div>
-        <div className="row align-items-center">
-          <div className="contenedor__autorSagas col-3 g-4 px-4">
-            <img
-              src={
-                imagenesAutores.find(({ id }) => Number(id) === datosAutor.id)
-                  ? imagenesAutores.find(
-                      ({ id }) => Number(id) === datosAutor.id
-                    ).ruta
-                  : noDisponible
-              }
-              alt={`Foto de ${datosAutor.nombre}`}
-            />
-          </div>
-          <div className="col-9 g-4">
-            <p>{datosAutor.biografia} </p>
-          </div>
-        </div>
+      ) : (
+        <p>Cargando datos ...</p>
+      )}
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+        {libros ? <ItemList libros={libros} /> : <Loader />}
+      </div>
+      <div className="row mt-5">
+        <h2>Sobre el autor</h2>
+        {datosSaga ? (
+          <AutorEnSagas autor={datosSaga[0].autor.id} />
+        ) : (
+          <Loader />
+        )}
       </div>
     </section>
   );
