a31334adcbed693b473d613fe40ed49145f43190
Amneris Calle <amneris.calle@gmail.com>
2024-01-15 21:36:36 -0300

    cambios en config files para el deploy a github

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <CartProvider>
-        <BrowserRouter basename="/mw-libros">
+        <BrowserRouter basename="/proyecto-React">
           <NavBar onQueryBusqueda={setQueryBusqueda} />
 
           <Routes>
